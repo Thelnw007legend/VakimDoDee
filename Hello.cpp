d5498bb9fede9ac0814d27415b3720226df5c69e
Thelnw007legend <139094208+Thelnw007legend@users.noreply.github.com>
2023-07-10 11:44:42 +0700

    Add files via upload

--- Hello.cpp
+++ Hello.cpp
@@ -6,8 +6,11 @@ int main()
 	cout<<"    Division : Information Technology."<<endl;
 	cout<<"        Year : 1."<<endl;
 	cout<<"        Room : RB."<<endl;
-	cout<<"         Sex : Male."<<endl;
-	cout<<"         Age : 18."<<endl;
+	cout<<"        Sex  : Male."<<endl;
+	cout<<"        Age : 18."<<endl;
+	cout<<"A = \101 =\x41 \n";
+	cout<<"\x56\x61\x6b\x69\x6d\n";
+	cout<<" I love u \t \\ \\ sus sus \\ \\ \n";
 	system("pause");
     return (0);
 }
