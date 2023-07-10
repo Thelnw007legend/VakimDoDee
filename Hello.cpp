5c35796f4aacedb60db8300375e47b27639a0fc4
Thelnw007legend <139094208+Thelnw007legend@users.noreply.github.com>
2023-07-10 10:38:34 +0700

    Add files via upload

---
+++ Hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{   cout<<"Student Code : 6606021631025."<<endl;
+	cout<<"Student Name : Jirapat Singka."<<endl;
+	cout<<"    Division : Information Technology."<<endl;
+	cout<<"        Year : 1."<<endl;
+	cout<<"        Room : RB."<<endl;
+	cout<<"         Sex : Male."<<endl;
+	cout<<"         Age : 18."<<endl;
+	system("pause");
+    return (0);
+}
