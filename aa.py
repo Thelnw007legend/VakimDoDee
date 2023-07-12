f5027278da636ba6105ede17d9175cefdb01696f
Thelnw007legend <139094208+Thelnw007legend@users.noreply.github.com>
2023-07-12 16:29:27 +0700

    Add files via upload

---
+++ aa.py
@@ -0,0 +1,39 @@
+name = input("Enter name :")
+surname = input("Enter surname :")
+age = int(input("Enter age :"))
+graduate = age + 4
+
+
+
+
+print("My name is" ,name)
+print("My Surname is",surname)
+print("Now i'm",age,"years old.")
+print("I'll finish undergraduate in",graduate,"years old.") 
+
+
+
+
+num1 = int(input("Enter integer number 1 :"))
+num2 = int(input("Enter integer number 2 :"))
+num3 = int(input("Enter integer number 3 :")) 
+
+
+
+
+print("Value Number :",num1)
+print("Value Number :",num2)
+print("Value Number :",num3)
+print("Total all :",num1+num2+num3)
+
+
+
+
+Num1 = float(input("Enter float number 1 :"))
+Num2 = float(input("Enter float number 2 :"))
+Num3 = float(input("Enter float number 3 :"))
+
+
+
+
+print('Value number 1 :',Num1,'\nValue number 2 :',Num2,'\nValue number 3 :',Num3,'\nTotal all :',Num1+Num2+Num3)
