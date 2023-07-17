a44f0b2e5b1230f7f1f68c9b1f3624bbaa19143e
Jirapat Singka <139094208+Thelnw007legend@users.noreply.github.com>
2023-07-17 11:42:02 +0700

    Add files via upload

---
+++ Dk.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{	
+	float f;
+	cout<<"Enter Fahrenheit :";
+	cin>>f;
+	cout<<"**********Output**********"<<endl;
+	cout<<"Celcuis = "<<95.0/9.0)*(f-32)<<endl;
+	system("pause");
+	return(0);
+}
