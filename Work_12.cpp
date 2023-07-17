0eb791a3c7992b9ff09c3bfbbcd22817acb05c2e
Jirapat Singka <139094208+Thelnw007legend@users.noreply.github.com>
2023-07-17 10:37:57 +0700

    Add files via upload

---
+++ Work_12.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	string Name;
+	int Year;
+	cout<<"Enter name : ";
+	cin>>Name;
+	cout<<"Enter Year of birth : ";
+	cin>>Year;
+	cout<<"**********Output**********\n";
+	cout<<"Your age : "<< 2566-Year <<" years old "<<endl;
+	system("pause");
+	return(0);
+}
