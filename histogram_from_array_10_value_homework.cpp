9e7b352e03613223daef0e9fa7de136888516a6c
Jirapat Singka <139094208+Thelnw007legend@users.noreply.github.com>
2023-09-24 22:25:24 +0700

    Add files via upload

---
+++ histogram_from_array_10_value_homework.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <cstdlib> 
+#include <ctime>   
+
+using namespace std;
+
+void fillRandom(int arry[10]) {
+    for (int i = 0; i < 10; i++) {
+        arry[i] = rand() % 10 + 1; 
+    }
+}
+
+int main() {
+    int arry[10];
+    
+    srand(time(0)); 
+
+    fillRandom(arry); 
+    
+    cout<< "Element\t" << "Value\t" << "Histogram\t" << endl;
+
+    for (int i = 0; i < 10; i++) {
+        cout<< i << "\t" << arry[i] << " \t";
+        
+        for (int j = 1; j <= arry[i]; j++) {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    system("pause"); 
+    return 0;
+}
